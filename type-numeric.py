d9c8fb7f5d2a29a1b5346531ba47462b28a55b83
Takehiko OGASAWARA <take_oga@hotmail.com>
2023-05-05 23:42:14 +0900

    string, list, tupleサンプル追加

--- type-numeric.py
+++ type-numeric.py
@@ -5,17 +5,16 @@
 
 from decimal import *
 
-'{:d}'.format(0b11) # 10進数で表示
-'{:d}'.format(0o11)
-'{:d}'.format(0x11)
-
+print ('{:d}'.format(0b11)) # 2進数 -> 10進数で表示
+print ('{:d}'.format(0o11)) # 8進数 -> 10進数で表示
+print ('{:d}'.format(0x11)) # 16進数 -> 10進数で表示
 
 m = 123
-type(m)
+print(type(m))
 
 
 ## False
-0.1 + 0.1 + 0.1 == 0.3
+print(0.1 + 0.1 + 0.1 == 0.3)
 
 ## True
-Decimal('0.1') + Decimal('0.1') + Decimal('0.1') == Decimal('0.3')
+print(Decimal('0.1') + Decimal('0.1') + Decimal('0.1') == Decimal('0.3'))
